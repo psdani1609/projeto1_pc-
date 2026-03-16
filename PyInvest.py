2822812362cc7ccf7970f7dbc9dfb0f7949378fd
danirado_ <danielmsouza002@gmail.com>
2026-03-16 19:47:04 -0300

    Update PyInvest.py

--- PyInvest.py
+++ PyInvest.py
@@ -37,7 +37,45 @@ MontanteLCI = (Capital * math.pow(1+ TaxaLCI, Meses) + (Aporte * Meses))
 
 #Poupança
 TaxaPoupanca = 0.005
-MontantePoupanca = (Capital * math.pow(1 + TaxaPoupanca) ,Meses + (Aporte * Meses))
+MontantePoupanca = (Capital * math.pow(1 + TaxaPoupanca , Meses) + (Aporte * Meses))
 
-#FII - Simulações
+#Simulações
+Simulacao1 = (Capital * math.pow((TaxaFundoInvestimentoImobiliario + random.uniform (-0.03,0.03) ) , Meses) + (Aporte * Meses))
+Simulacao2 = (Capital * math.pow((TaxaFundoInvestimentoImobiliario + random.uniform (-0.03,0.03) ) , Meses) + (Aporte * Meses))
+Simulacao3 = (Capital * math.pow((TaxaFundoInvestimentoImobiliario + random.uniform (-0.03,0.03) ) , Meses) + (Aporte * Meses))
+Simulacao4 = (Capital * math.pow((TaxaFundoInvestimentoImobiliario + random.uniform (-0.03,0.03) ) , Meses) + (Aporte * Meses))
+Simulacao5 = (Capital * math.pow((TaxaFundoInvestimentoImobiliario + random.uniform (-0.03,0.03) ) , Meses) + (Aporte * Meses))
+
+ValorFundoInvestimentoImobiliario = {Simulacao1, Simulacao2, Simulacao3, Simulacao4, Simulacao5}
+
+MediaFII = statistics.mean (ValorFundoInvestimentoImobiliario)
+MedianaFii = statistics.median (ValorFundoInvestimentoImobiliario)
+DesvioPadraoFII = statistics.stdev (ValorFundoInvestimentoImobiliario)
+
+ValorFinalFii = MediaFII
+
+#Simulação das datas
+
+DataSimulacao = datetime.datetime.now()
+DiasParaResgate = Meses * 30
+DataParaResgate = DataSimulacao + datetime.timedelta(days = DiasParaResgate)
+
+#MetaFinanceira
+
+MetaAtingida = ValorFinalFii >= Meta
+
+#Formatação/Saidaseilanaosei
+
+CapitalF = locale.currency(Capital,grouping=True)
+TotalInvestidoF = locale.currency(TotalInvestido,grouping=True)
+CdbF = locale.currency(MontanteCDB_Liquido,grouping=True)
+LciF = locale.currency(MontanteLCI,grouping=True)
+PoupancaF = locale.currency(MontantePoupanca,grouping=True)
+FiiF = locale.currency(ValorFinalFii,grouping=True)
+MediaFiiF = locale.currency(MediaFII,grouping=True)
+DesvioPadraoFIIF = locale.currency(DesvioPadraoFII,grouping=True)
+
+#Gráfico
+
+
 
